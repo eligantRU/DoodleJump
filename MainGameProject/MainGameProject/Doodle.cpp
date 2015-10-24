e12b2f52408d77efcdaf8ed94b41b7287495b23d
eligant_ru <eligant.ru@gmail.com>
2015-10-25 02:04:30 +0400

    Added files [Doodle Jump]

---
+++ MainGameProject/MainGameProject/Doodle.cpp
@@ -0,0 +1,12 @@
+void initialHero(Game & game);
+
+void initialHero(Game & game)
+{
+	Doodle & hero = game.hero;
+
+	hero.body = new sf::RectangleShape(sf::Vector2f(40, 70));
+	hero.body->setFillColor(sf::Color(0, 8 * 16 + 4, 16 + 7));
+	hero.body->setPosition(260, 70);
+	hero.direction.x = NONE;
+	hero.jumping = false;
+}
