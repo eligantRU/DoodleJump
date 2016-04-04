4fe953b2ce6247b998eaa167b2a2d917e2cda1b7
eligantRU <eligant.ru@gmail.com>
2016-04-04 17:10:32 +0400

    Refactoring: changed file names
    Signed-off-by: eligantRU <eligant.ru@gmail.com>

--- DoodleJump/DoodleJump/GameScene.h
+++ DoodleJump/DoodleJump/GameScene.h
@@ -3,14 +3,48 @@
 #include "stdafx.h"
 #include "sheet.h"
 
-void onGameFrame(sf::RenderWindow &);
+struct gameScene
+{
+public:
+	gameScene();
+	~gameScene();
+	gameResult onGameFrame(sf::RenderWindow & window);
+private:
+	Bonus bonus[NUMBER_BONUSES];
+	Plate plate[NUMBER_PLATES];
+	Doodle hero;
+	sf::Sprite * hole;
+	bool endOfGame;
+	void render(sf::RenderWindow & window);
+	void initBonuses(void);
+	void moveBonuses(void);
+	void generBonuses(void);
+	void update(sf::RenderWindow & window);
+	void keyPressed(sf::RenderWindow & window);
+	float checkDoodleFall(void);
+	void dropUnstablePlates(void);
+	void generHole(void);
+	bool checkGameEnd(void);
+	void checkCylinderEffect(sf::Vector2f & doodlePosition);
+	Collision checkCollisionPlate(sf::Vector2f & doodlePosition, sf::Vector2f platePosition[NUMBER_PLATES]);
+	Collision checkCollisionBonus(sf::Vector2f & doodlePosition, sf::Vector2f bonusPosition[NUMBER_PLATES]);
+	Collision checkCollisionHole(sf::Vector2f & doodlePosition);
+	void buildBonus(BonusType bonusType, int bonusIndex, sf::Vector2f platePosition, int plateIndex);
+	void moveDynamicPlates(void);
+	void generPlates(void);
+	int qwerty; // TODO: What? R u crazy?
+	sf::Text text;
+	uint64_t points;
+	BonusType actualBonus;
+	int actualBonusId;
+	sf::Vector2f holePosition;
+	int unstablePlatesCounter;
+	sf::Sprite * background;
+	std::vector<sf::Vector2f> platePosition;
+	bool isPause;
+	gameResult result;
+	void resetGame(void);
+	sf::Vector2f offsetFallBonus;
 
-void gameLoop(sf::RenderWindow & window, Game & game);
-
-void resetGame(Game & game, sf::View & view); 
-
-void enterGameLoop(void);
-
-void update(sf::RenderWindow & window, Game & game, sf::View & view);
-
-void initAssets(Game & game);
+	// Enemy * enemy[NUMBER_ENEMY];
+};
